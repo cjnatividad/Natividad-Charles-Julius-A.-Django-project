6a6344b7bbc4fdf0796c5d66de555c10206e6de2
cjnatividad <natividadrjay99@gmail.com>
2023-03-10 21:23:18 +0800

    Added folder to git

--- urls.py
+++ urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ccmsMission',views.dMission, name='mission'),
+    path('ccmsVission',views.dVission, name='vision'),
+    path('ccmsObjectives',views.dobjects, name='objectives'),
 ]
